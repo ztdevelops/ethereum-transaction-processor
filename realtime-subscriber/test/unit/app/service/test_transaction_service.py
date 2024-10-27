93218907d85c5a2d9fb108fa7320b429075594a1
ztdevelops <yuezhengting@gmail.com>
2024-10-27 17:18:58 +0800

    fix: add missing files from previous commit

--- realtime-subscriber/test/unit/app/service/test_transaction_service.py
+++ realtime-subscriber/test/unit/app/service/test_transaction_service.py
@@ -3,13 +3,15 @@ from unittest.mock import MagicMock
 
 from src.app.dto.response.healthcheck_response_dto import HealthcheckResponseDTO
 from src.app.repository.transaction_repository import TransactionRepository
+from src.app.service.etherscan_service import EtherscanService
 from src.app.service.transaction_service import TransactionService
 
 
 class TestTransactionService(unittest.TestCase):
     def setUp(self):
         self.transaction_repository = MagicMock(spec=TransactionRepository)
-        self.transaction_service = TransactionService(self.transaction_repository)
+        self.etherscan_service = MagicMock(spec=EtherscanService)
+        self.transaction_service = TransactionService(self.transaction_repository, self.etherscan_service)
 
     def test_perform_healthcheck(self):
         # Arrange
